760a503d2dc66a57c0529bd343f8ad1498d90b70
Timon Burkard <timon.burkard@gwf.ch>
2025-02-05 12:52:23 +0100

    [project/firmware/buttons] added buttons
    Signed-off-by: Timon Burkard <timon.burkard@gwf.ch>

--- project/firmware/Core/Src/main.c
+++ project/firmware/Core/Src/main.c
@@ -302,12 +302,24 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_RESET);
 
+  /*Configure GPIO pins : BUTTON_DOWN_Pin BUTTON_UP_Pin BUTTON_RIGHT_Pin */
+  GPIO_InitStruct.Pin = BUTTON_DOWN_Pin|BUTTON_UP_Pin|BUTTON_RIGHT_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
   /*Configure GPIO pins : BUZZER_Pin MAX_SPI_CS_Pin */
   GPIO_InitStruct.Pin = BUZZER_Pin|MAX_SPI_CS_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : BUTTON_CENTER_Pin BUTTON_LEFT_Pin */
+  GPIO_InitStruct.Pin = BUTTON_CENTER_Pin|BUTTON_LEFT_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
   /*Configure GPIO pin : LED_GREEN_Pin */
   GPIO_InitStruct.Pin = LED_GREEN_Pin;
